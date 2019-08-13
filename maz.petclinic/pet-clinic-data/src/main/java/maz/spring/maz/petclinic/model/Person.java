7bbfed4d5d15b4bc3856bc9ed686c8189ebb83cf
ayoubzoubeidi <ayoubzoubeidi@gmail.com>
2019-08-13 10:43:26 +0100

    adding BaseEntity.

--- maz.petclinic/pet-clinic-data/src/main/java/maz/spring/maz/petclinic/model/Person.java
+++ maz.petclinic/pet-clinic-data/src/main/java/maz/spring/maz/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package maz.spring.maz.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- maz.petclinic/pet-clinic-data/src/main/java/maz/spring/maz/petclinic/model/PetType.java
+++ maz.petclinic/pet-clinic-data/src/main/java/maz/spring/maz/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package maz.spring.maz.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
