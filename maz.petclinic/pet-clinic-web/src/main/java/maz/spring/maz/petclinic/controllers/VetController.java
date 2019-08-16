fc1d45a75b969b76587f3f6e9d10e6858bfbd4a1
ayoubzoubeidi <ayoubzoubeidi@gmail.com>
2019-08-16 18:15:29 +0100

    List Vets on Vets Index Page. closes #13

--- maz.petclinic/pet-clinic-web/src/main/java/maz/spring/maz/petclinic/controllers/VetController.java
+++ maz.petclinic/pet-clinic-web/src/main/java/maz/spring/maz/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package maz.spring.maz.petclinic.controllers;
 
+import maz.spring.maz.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
